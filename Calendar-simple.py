cfca8f0ad645f259641a8b619de3145123f36901
Jonas <36644388+QC20@users.noreply.github.com>
2023-08-09 00:16:18 +0200

    Add files via upload

---
+++ Calendar-simple.py
@@ -0,0 +1,11 @@
+import calendar
+import datetime
+
+# Get the current date and time
+now = datetime.datetime.now()
+
+yy = now.year  # year
+mm = now.month  # month
+
+# Display the calendar
+#print(calendar.month(yy, mm))
